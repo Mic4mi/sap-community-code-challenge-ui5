95f071932eb75871e77a00fe47678959e030a783
Acuña <micaela.paz.acuna@accenture.com>
2022-07-19 20:52:37 -0300

    new test on week 3

--- webapp/test/e2e/week3.test.js
+++ webapp/test/e2e/week3.test.js
@@ -2,6 +2,27 @@ const { wdi5 } = require("wdio-ui5-service")
 const Detail = require("./pageObjects/Detail")
 
 describe("week3: detail page", () => {
+    const oInputSelector = {
+        selector: {
+            id: "nameInput",
+            viewName: Detail._viewName
+        }
+    }
+
+    const oButtonSelector = {
+        selector: {
+            id: "sendNameBtn",
+            viewName: Detail._viewName
+        }
+    }
+
+    const oTextSelector = {
+        selector: {
+            id: "nameText",
+            viewName: Detail._viewName
+        }
+    }
+
     before(async () => {
         await Detail.open()
     })
@@ -11,22 +32,12 @@ describe("week3: detail page", () => {
         expect(title).toEqual("ui5-challenge")
     })
 
-    it("should have button to navigate back", async () => {
-        const navButton = await browser.asControl({
-            selector: {
-                id: /.*navButton$/,
-                viewName: Detail._viewName
-              }
-        })
-        expect(await navButton.getVisible()).toBeTruthy()
-    })
-
     it("should have button that opens dialog", async () => {
         const dialogButton = await browser.asControl({
             selector: {
                 id: "dialogButton",
                 viewName: Detail._viewName
-              }
+            }
         })
         await dialogButton.firePress()
 
@@ -35,31 +46,39 @@ describe("week3: detail page", () => {
         expect(dialog.error).toBeFalsy()
     })
 
-    it("After typing in the input, the button should make the input value appear as text", async () => {
-        const oInput = await browser.asControl({
-            selector: {
-                id: "nameInput",
-                viewName: Detail._viewName
-            }
-        });
+    it("after typing in the input, the button should make the input value appear as text", async () => {
+        const oInput = await browser.asControl(oInputSelector);
         await oInput.setValue('Mica');
 
-        const oButton = await browser.asControl({
-            selector: {
-                id: "sendNameBtn",
-                viewName: Detail._viewName
-            }
-        });
+        const oButton = await browser.asControl(oButtonSelector);
         await oButton.firePress();
 
-        const oText = await browser.asControl({
-            selector: {
-                id: "nameText",
-                viewName: Detail._viewName
-            }
-        });
+        const oText = await browser.asControl(oTextSelector);
         const sText = await oText.getText();
 
         expect(sText).toBe('Mica');
+    })
+
+    it("if you send empty text in the input and press the button, a warning should appear", async () => {
+        const oInput = await browser.asControl(oInputSelector);
+        await oInput.setValue('');
+
+        const oButton = await browser.asControl(oButtonSelector);
+        await oButton.firePress();
+
+        const oText = await browser.asControl(oTextSelector);
+        const sText = await oText.getText();
+
+        expect(sText).toBe('It seems that there is no name...');
+    })
+
+    it("should have button to navigate back", async () => {
+        const navButton = await browser.asControl({
+            selector: {
+                id: /.*navButton$/,
+                viewName: Detail._viewName
+            }
+        })
+        expect(await navButton.getVisible()).toBeTruthy()
     })
 })
