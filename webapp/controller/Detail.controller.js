e401832b7639d69c9affeb1a6c8eedcc3f28aad2
Acuña <micaela.paz.acuna@accenture.com>
2022-07-19 16:07:44 -0300

    specs 1,2,3 & half 4

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,10 +1,36 @@
 sap.ui.define(
-    ["./BaseController", "sap/m/MessageToast", "sap/ui/core/routing/History",],
+    [
+        "./BaseController",
+        "sap/m/MessageToast",
+        "sap/ui/core/routing/History",
+        "sap/m/Dialog",
+        "sap/m/Button",
+        "sap/m/library",
+        "sap/m/List",
+        "sap/m/StandardListItem",
+        "sap/m/Text"
+    ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast, History) {
+    function (
+        Controller,
+        MessageToast,
+        History,
+        Dialog,
+        Button,
+        mobileLibrary,
+        List,
+        StandardListItem,
+        Text
+    ) {
         "use strict";
+
+        // shortcut for sap.m.ButtonType
+        var ButtonType = mobileLibrary.ButtonType;
+
+        // shortcut for sap.m.DialogType
+        var DialogType = mobileLibrary.DialogType;
 
         return Controller.extend("ui5.challenge.controller.Detail", {
             onInit: function () {
@@ -19,7 +45,32 @@ sap.ui.define(
                 } else {
                     this.getRouter().navTo("Main", {}, true /*no history*/);
                 }
-            }
+            },
+
+            onOpenDialog: function (oEvent) {
+                if (!this.oDefaultDialog) {
+                    this.oDefaultDialog = new Dialog({
+                        id: "myDialog",
+                        title: "Hello friend!",
+                        type: "Message",
+                        content: new Text({
+                            text: "Hello",
+                            class: "sapUiMediumMarginBegin"
+                        }),
+                        endButton: new Button({
+                            text: "Close",
+                            press: function () {
+                                this.oDefaultDialog.close();
+                            }.bind(this)
+                        })
+                    });
+
+                    // to get access to the controller's model
+                    this.getView().addDependent(this.oDefaultDialog);
+                }
+
+                this.oDefaultDialog.open();
+            },
         });
     }
 );
--- webapp/test/e2e/week4.test.js
+++ webapp/test/e2e/week4.test.js
@@ -7,12 +7,10 @@ You have all done a great job so far in building an app that passes predefined t
 Now it's time to take it one step further and write your own tests 🚀
 For week 4 the task is to write a minimum of 2 tests and make them pass ✅✅
 There are no boundaries, get creative and explore the framework 🎨
-
 You can get inspiration here:
     🗓  test files of the previous weeks
     ℹ️  examples in wdi5 repository: https://github.com/js-soft/wdi5/tree/main/examples/ui5-js-app/e2e-test-config
     📚  official wdi5 documentation: https://js-soft.github.io/wdi5/#/
-
 You can share your results and experience in the SAP Community 💙
 https://groups.community.sap.com/t5/coffee-corner-discussions/sap-community-code-challenge-testing-ui5-apps-with-wdi5/td-p/5229
 */
@@ -24,11 +22,35 @@ describe("week4: main or detail page ...", () => {
         await Detail.open()
     })
 
-    it("", async () => {
-        // your first test
+    it("the searchfield should filter the table", async () => {
+        // Primero, debemos traer el searchfield, insertarle un valor y disparar su evento        
+        const Searchfield = await browser.asControl({
+            selector: {
+                id: "mySearchfield",
+                viewName: Main._viewName
+            }
+        });
+
+        await Searchfield.setValue('18');
+        await Searchfield.fireLiveChange();
+
+        // //Debemos comprobar que los items de la tabla se hayan filtrado
+        const Table = await browser.asControl({
+            selector: {
+                viewName: Main._viewName,
+                id: "idProductsTable"
+            }
+        })
+
+        const TableItems = await Table.getItems();
+        expect(TableItems.length).toBe(1)
     })
 
-    it("", async () => {
-        // your second test
-    })
+    // it("", async () => {
+    //     // your first test
+    // })
+
+    // it("", async () => {
+    //     // your second test
+    // })
 })
